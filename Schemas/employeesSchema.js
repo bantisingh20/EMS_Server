fc7f08f11e2f12c3e9810f0426731265c2e5b36c
Banti Singh <banti.singh@mysetu.com>
2024-10-10 17:51:57 +0530

    designation set up

--- Schemas/employeesSchema.js
+++ Schemas/employeesSchema.js
@@ -1,26 +1,33 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
+const bcrypt =require('bcrypt');
+const {UserTable} = require("../Schemas/user");
 
 const EmployeeSchema = new mongoose.Schema({
 employeeid : {type:Number ,required : true ,default:0 },
-employeecode : {type:String,required :true, default:''},
+employeecode : {type:String,required :true, default:'' },
+//isuser:{type:String,required:true},
+Userid :{type: mongoose.Schema.Types.ObjectId,ref:"User"},
 firstname : { type:String , required : true },
 lastname : { type : String , required :true},
 contactno : {type:Number},
 emailid: {type:String, required: true },
-gender :{type:String},
-dateofbith :{type:String},
-department : { type: String , required :true},
-designation : { type: String , required :true},
+//password:{ type:String},
+dateofbith :{type:Date},
 dateofJoining : {type:Date,required:true},
+gender :{type:String},
+//maritialstatus :{type:String},
+department : { type: mongoose.Schema.Types.ObjectId,ref:"department" , required :true},
+designation : { type: String , required :true},
+//employeetype: {type:String},
 address :{ type:String},
-employeetype: {type:String},
 activestatus:{type: String, enum: ['n', 'y'] ,default: 'y'},
 recordstatus: { type: String, required: true, default: 'insert' },
 statusdate: { type: Date, default: Date.now },
 disabled: { type: String, enum: ['n', 'y'], required: true, default: 'n' },
 isdeleted: { type: String, enum: ['n', 'y'], required: true, default: 'n' },
-isUser :{type: Boolean,enum :['n','y'] ,default:'y'},
+
 
 })
 
@@ -28,19 +35,64 @@ isUser :{type: Boolean,enum :['n','y'] ,default:'y'},
 EmployeeSchema.plugin(AutoIncrement, { inc_field: 'employeeid' });
 const EmployeeTable = mongoose.model("Employee",EmployeeSchema)
 
+const storage = multer.diskStorage({
+    destination :(req,file,cb) =>{
+        cb(null,"public/UploadDocument")
+    },
+    filename:(req,file,cb) =>{
+        cb(null,Date.now() +path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage:storage})
+
 const SaveEmployee = async(req,res) => {
 
     try{ 
 
-        const {emailid} = req.body;
+        const {employeecode,firstname,lastname,contactno,emailid,dateofbith,dateofJoining,
+            gender,department,designation,address,password,role
+        } = req.body;
         console.log(req.body);
 
-        const employeExist = await EmployeeTable.find( {emailid})
+        const employeExist = await EmployeeTable.findOne( {emailid})
+        console.log(employeExist);
         if(employeExist){
-            res.status(400).json({success:false , message:"Employee Exist" });
+           return res.status(400).json({success:false , message:"Employee Exist" });
         } 
+        
 
-        const NewEmployee = EmployeeTable(req.body);
+        console.log(password);
+        const haspassword = await bcrypt.hash(password,10);
+        const newUser = new UserTable({
+            name : `${firstname} ${lastname}`,
+            email:emailid,
+            password:haspassword,
+            role:"admin",
+            profileImage:req.file ? req.file.name : "",
+            disabled:"n"
+
+        })
+
+       
+        const newSavedUserid = await newUser.save();
+
+        console.log(newSavedUserid);
+        const NewEmployee = EmployeeTable({
+            employeecode,
+            Userid:newSavedUserid._id,
+            employeecode
+            ,firstname
+            ,lastname
+            ,contactno
+            ,emailid
+            ,dateofbith
+            ,dateofJoining
+            ,gender
+            ,department
+            ,designation
+            ,address
+        });
         await NewEmployee.save();
         res.status(200).json({ success: true, message: "Employee Saved", data: NewEmployee });
     }
@@ -58,4 +110,4 @@ const GetAllEmployees = async(req,res) => {
         console.log(error);
     }
 }
-module.exports = {EmployeeTable ,SaveEmployee}
+module.exports = {EmployeeTable ,SaveEmployee,upload}
